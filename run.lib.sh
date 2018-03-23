4eb1ec782fb487b88fa0b80a50b446419a0b55eb
Tomer <shaolintl@gmail.com>
2018-03-23 21:05:53 +0100

    working now but check if needing 4 instantiations of the box!

--- run.lib.sh
+++ run.lib.sh
@@ -13,9 +13,6 @@ RUNNER_SIG_TARGET=$SRCDIR/${MODNAME}.sig
 }
 
 function cleanup(){
-rm -f $SRCDIR/kernel/lmf/lmf-kernel.lpo $SRCDIR/kernel/lmf/lmf-kernel.lp $SRCDIR/kernel/lmf/lmf-kernel.mod
-cp $TOOLS/kernel_debug/lmf-kernel.mod$DEBUG $SRCDIR/kernel/lmf/lmf-kernel.mod
-
 rm -f $RUNNER_MOD_TARGET
 rm -f $RUNNER_SIG_TARGET
 }
--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ if [ -z "$NO_PREPARE" ]; then
 
   echo -e "module  ${MODNAME}.\\n" >> $RUNNER_MOD_TARGET
   `cat $RUNNER_MOD_BASE >> $RUNNER_MOD_TARGET`
-  echo " ($1) (interact L_ $2 _ e [])." >> $RUNNER_MOD_TARGET
+  echo " (lform z $1) (interact L_ $2 _ e [] [])." >> $RUNNER_MOD_TARGET
 
   echo 'main :- run.' >> $RUNNER_MOD_TARGET
 fi
